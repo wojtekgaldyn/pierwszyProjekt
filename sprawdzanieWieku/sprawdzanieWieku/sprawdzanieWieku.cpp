54be74f2be2a4d86fa49070ee5a13d742f645583
wojtekG <wojtekG@gmail.com>
2026-09-08 10:41:15 +0200

    Dodanie prostego projektu i modyfikacja README

---
+++ sprawdzanieWieku/sprawdzanieWieku/sprawdzanieWieku.cpp
@@ -0,0 +1,24 @@
+﻿// sprawdzanieWieku.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int podanyWiek;
+    cout << "Podaj wiek\n";
+    cin >> podanyWiek;
+    if (podanyWiek >= 4 && podanyWiek <= 6) {
+        cout << "Chodzisz do przedszkola\n";
+    }
+    else if (podanyWiek >= 7 && podanyWiek <= 15) {
+        cout << "Chodzisz do szkoly podstawowej\n";
+    }
+    else if (podanyWiek >= 16 && podanyWiek <= 19) {
+        cout << "Chodzisz do szkoly sredniej\n";
+    }
+
+}
+
+
